1beb878b7868cf3b27f86ebab58f35805d01160b
Syed Muhammad Tahir Hasan <thjafri11@gmail.com>
2022-07-23 17:52:22 +0200

    added linspace fucntion

--- test_new.py
+++ test_new.py
@@ -6,10 +6,13 @@ import numpy as np
 a = np.arange(10)
 print(a)
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-x=['2011','2012','2013','2014','2015']
-y = [1,2,3,4,5]
+# x=['2011','2012','2013','2014','2015']
+# y = [1,2,3,4,5]
 
-plt.plot(x,y)
-plt.show()
+# plt.plot(x,y)
+# plt.show()
+
+b = np.linspace(10, 100, 10)
+print(b)
