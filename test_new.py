b40d88d49a6bb54ee215b8d46f2db9d383f2c39f
Syed Muhammad Tahir Hasan <thjafri11@gmail.com>
2022-07-23 17:46:30 +0200

    updated test_new.py

--- test_new.py
+++ test_new.py
@@ -1,1 +1,15 @@
-print('Hello World')
+# print('Hello World')
+
+import sklearn
+import numpy as np
+
+a = np.arange(10)
+print(a)
+
+import matplotlib.pyplot as plt
+
+x=['2011','2012','2013','2014','2015']
+y = [1,2,3,4,5]
+
+plt.plot(x,y)
+plt.show()
